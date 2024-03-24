6284a1335c4377079d1329f080383171dbb1ee7f
rajayash <62595165+rajayash@users.noreply.github.com>
2024-03-24 16:03:59 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import "./styles.css";
+import reactLogo from "./demo-image-0.png";
+
+export default function App() {
+  return (
+    <div className="app">
+      <h1>Image in project</h1>
+      <img src={reactLogo} alt="react logo" />
+
+      <h1>Image from google</h1>
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png"
+        alt="react logo"
+      />
+      <h1>Background image</h1>
+      <div
+        className="background-image"
+        style={{
+          backgroundImage:
+            "url(https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png)",
+        }}
+      >
+        Overlay text
+      </div>
+    </div>
+  );
+}
